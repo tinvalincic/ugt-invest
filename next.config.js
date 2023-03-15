aaeabd1c927828ec7d02cb1578b0a108c6bf5db2
Tin Valinčić <tin.valincic@minus5.hr>
2023-03-15 19:28:52 +0100

    fix build config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: ".",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
