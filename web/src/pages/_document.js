22e0a8141237e93d6950ec4a0f6762aab1832f51
tinvalincic <tinvalinc@gmail.com>
2023-03-28 21:33:20 +0200

    add google analytics

--- web/src/pages/_document.js
+++ web/src/pages/_document.js
@@ -1,9 +1,28 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-ES68P27RRS"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-ES68P27RRS');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
